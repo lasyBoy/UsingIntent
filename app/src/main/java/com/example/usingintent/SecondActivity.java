9ede1b5480e07ae02664cf21f5a4dcf8b5d62f22
Emmanuel <emakouamou@yahoo.fr>
2019-03-07 20:08:29 +0100

    Initial commit

---
+++ app/src/main/java/com/example/usingintent/SecondActivity.java
@@ -0,0 +1,10 @@
+package com.example.usingintent;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
